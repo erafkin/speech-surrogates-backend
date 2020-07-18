c18e68ea6cff689827bf5a750fe84ec9aa05b3d0
Emma Rafkin <emmarafkin@gmail.com>
2020-07-18 12:38:45 -0500

    news model and controller

---
+++ src/controllers/news-controller.js
@@ -0,0 +1,48 @@
+import News from '../models/news-model';
+
+export const getAllKeywords = () => {
+    return new Promise((resolve, reject) => {
+        News.find()
+        .then((b)=> {
+            if (b !== null) {
+                resolve(b);
+              } else {
+                reject({ code: RESPONSE_CODES.INTERNAL_ERROR, error });
+              }
+            }).catch((error) => {
+              reject(error);
+            });
+        
+        }) 
+}
+export const createNews = (news) => {
+    return new Promise((resolve, reject)=>{
+        if (!(news)) {
+			reject({
+				code: RESPONSE_CODES.BAD_REQUEST,
+				error: { message: 'Please provide a news object' },
+			});
+        }
+        News.create({
+            blurb: news.blurb,
+            link: news.link ? news.link : null
+        }).then((result) => {
+            resolve(result)
+        }).catch((error) => {
+            reject({ code: RESPONSE_CODES.INTERNAL_ERROR, error: error });
+        })
+    });
+}
+
+export const deleteNews = (news) => {
+    return new Promise((resolve, result) => {
+        News.deleteOne(news).then((result)=> {
+            resolve(result)
+        }).catch((error) => {
+            reject({ code: RESPONSE_CODES.INTERNAL_ERROR, error: error });
+        })
+    })
+    
+}
+
+
---
+++ src/models/news-model.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+
+const NewsSchema = new Schema({
+    blurb: String,
+    link: String,
+}, {collection: "news"});
+
+NewsSchema.set('toJSON', {
+  virtuals: true,
+});
+
+const News = mongoose.model('News', NewsSchema);
+
+
+export default News;
